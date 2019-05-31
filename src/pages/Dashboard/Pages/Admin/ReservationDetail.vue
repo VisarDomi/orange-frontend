063535b223f45e62770a6b954eb08260332c6b19
Visar Domi <visardomi4@gmail.com>
2019-05-31 19:19:51 +0200

    another mid work commit

--- src/pages/Dashboard/Pages/Admin/ReservationDetail.vue
+++ src/pages/Dashboard/Pages/Admin/ReservationDetail.vue
@@ -1,105 +1,81 @@
 <template>
   <div class="content">
     <div class="md-layout">
-      <div class="md-layout-item md-medium-size-100 md-size-40 mx-auto">
-        <form>
-          <md-card>
-            <md-card-header class="md-card-header-icon">
-              <div class="card-icon">
-                <md-icon>card_travel</md-icon>
-              </div>
-              <h4 class="title">
-                Reservation -
-                <small>Details</small>
-              </h4>
-            </md-card-header>
-
-            <md-card-content>
-              <div class="md-layout md-gutter md-size-100">
-                <div class="md-layout md-layout-item md-small-size-100 md-size-100">
-                  <div class="md-layout-item md-small-size-100 md-size-100">
-                    <md-field>
-                      <label>Pickup</label>
-                      <md-input v-model="this.reservation.pickup" disabled></md-input>
-                    </md-field>
-                  </div>
-                  <div class="md-layout-item md-small-size-100 md-size-100">
-                    <md-field>
-                      <label>Destination</label>
-                      <md-input v-model="this.reservation.destination" disabled></md-input>
-                    </md-field>
-                  </div>
-                  <div class="md-layout-item md-small-size-100 md-size-100">
-                    <md-field>
-                      <label>Date</label>
-                      <md-input v-model="this.reservation.date" disabled></md-input>
-                    </md-field>
-                  </div>
-                  <div class="md-layout-item md-small-size-100 md-size-100">
-                    <md-field>
-                      <label>Time</label>
-                      <md-input v-model="this.reservation.time" disabled></md-input>
-                    </md-field>
-                  </div>
-
-                  <div class="md-layout-item md-small-size-100 md-size-100">
-                    <md-field>
-                      <label>Status</label>
-                      <md-input v-model="this.reservation.status" disabled></md-input>
-                    </md-field>
-                  </div>
-
-                  <!-- <div class="md-layout-item md-size-100 text-right">
-
-                  </div>-->
-                </div>
-              </div>
-            </md-card-content>
-          </md-card>
-        </form>
+      <div class="md-layout-item md-medium-size-100 md-size-66 mx-auto">
+        <edit-profile-form header-color="green"> </edit-profile-form>
       </div>
+      <!-- <div class="md-layout-item md-medium-size-100 md-size-33">
+        <user-card button-color="success"> </user-card>
+      </div> -->
+      			<div class="md-layout-item md-medium-size-100 md-size-66 mx-auto">
+
+				<md-button class="md-success" @click="createInvoice()">Create invoice for this reservation</md-button>
+			</div>
     </div>
+
   </div>
 </template>
 
 <script>
-import { UserCard } from "@/pages";
-import { GET_RESERVATION } from "@/store/actions.type";
-import { mapGetters } from "vuex";
+import { EditProfileForm, UserCard } from "@/pages";
+
+import { CREATE_INVOICE } from "@/store/actions.type";
 export default {
   name: "ReservationDetail",
   components: {
+    EditProfileForm,
     UserCard
   },
   data() {
-    return {
-      pickup: "",
-      destination: "",
-      date: "",
-      code: "",
-      time: "",
-      status: ""
-    };
+    return{
+      reservationId: 1,
+      invoice: {
+        "ref": "ref2",
+        "date": "2018-12-31",
+        "due": "2019-12-31",
+        "from_business_name": "adsf",
+        "from_addressline_1": "adsfasd",
+        "from_addressline_2": "wer",
+        "from_city": "werf",
+        "from_postcode": "sdf",
+        "from_vat": "sdsf",
+        "from_phone": "sdqwf",
+        "to_client_name": "sdaflk",
+        "to_addressline_1": "werjo",
+        "to_addressline_2": "wer",
+        "to_city": "werg",
+        "to_postcode": "edfg",
+        "to_vat": "sdwef",
+        "to_phone": "sdrf",
+        "payment_account_name": "weroui",
+        "payment_account_sortcode": "weroi",
+        "payment_account_number": "235890",
+        "invoice_notes": "aerguiopd",
+        "sub_total": "213",
+        "tax": "123",
+        "grand_total": "569"
+      }
+    }
   },
-  methods: {},
-  mounted() {
+  methods: {
+    createInvoice(){
+      //Send POST for creation of a new blank invoice
+      this.$store.dispatch(CREATE_INVOICE, {
+        reservationId: 1,
+        invoice: this.invoice
+      }).then(() => {//Then go to the page
+          console.log("Creating a new blank invoice...")
+          this.$router.push({ name: "CreateInvoice" });
+        });
+
+      
+      
+    }
+  },
+  mounted(){
     // this.$store.dispatch(ADMIN_GET_RESERVATION) //get reservation with store then store it in variable, then get it with mapGetters and plug it into POST invoice
-  },
-  created() {
-    this.$store
-      .dispatch(GET_RESERVATION, { reservationId: this.$route.params.id })
-      .then(() => {
-        this.pickup = this.reservation.pickup;
-        this.destination = this.reservation.destination;
-        this.date = this.reservation.date;
-        this.code = this.reservation.code;
-        this.time = this.reservation.time;
-        this.status = this.reservation.status;
-      });
-  },
-  computed: {
-    ...mapGetters(["reservation"])
   }
+
   //need map getter reservationId
 };
 </script>
--- src/store/modules/companyInvoice/actions.js
+++ src/store/modules/companyInvoice/actions.js
@@ -1,9 +1,9 @@
 import { CompanyInvoiceService } from "@/common/api.service";
+import { GET_COMPANY_INVOICES, GET_COMPANY_INVOICE } from "../../actions.type";
 import {
-  GET_COMPANY_INVOICE,
-  GET_COMPANY_INVOICES,
-} from "../../actions.type";
-import { SET_COMPANY_INVOICES, SET_COMPANY_INVOICE } from "../../mutations.type";
+  SET_COMPANY_INVOICES,
+  SET_COMPANY_INVOICE
+} from "../../mutations.type";
 
 export const actions = {
   async [GET_COMPANY_INVOICES](context, payload) {
@@ -18,10 +18,12 @@ export const actions = {
   async [GET_COMPANY_INVOICE](context, payload) {
     const { invoiceId } = payload;
     let companyId = UserStorageService.getUser().role_id;
-    await CompanyInvoiceService.getInvoice(companyId, invoiceId).then(({ data }) => {
-      console.log("Setting invoice data...");
-      context.commit(SET_COMPANY_INVOICE, data);
-      return data;
-    });
+    await CompanyInvoiceService.getInvoice(companyId, invoiceId).then(
+      ({ data }) => {
+        console.log("Setting invoice data...");
+        context.commit(SET_COMPANY_INVOICE, data);
+        return data;
+      }
+    );
   }
 };
