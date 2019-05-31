2cc89aac4bf3cb3177396e87988c36acf135b009
Visar Domi <visardomi4@gmail.com>
2019-05-31 10:38:13 +0200

    removed profile

--- src/store/modules/profile/getters.js
+++
@@ -1,3 +0,0 @@
-export const getters = {
-  profile: state => state.profile
-};
--- src/store/modules/profile/mutations.js
+++
@@ -1,8 +0,0 @@
-import { SET_PROFILE } from "../../mutations.type";
-
-export const mutations = {
-  [SET_PROFILE](state, profile) {
-    state.profile = profile;
-    state.errors = {};
-  }
-};
